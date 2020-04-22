06894a1ce2f42e2283d33f2f23c7ff70a01e6db3
themisAnagno <thmanagnostopoulos@gmail.com>
2020-04-22 00:09:12 +0000

    Create the spider

--- daily_wiki/spiders/articles.py
+++ daily_wiki/spiders/articles.py
@@ -1,11 +1,37 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import random
+
+from daily_wiki.items import DailyWikiItem
 
 
 class ArticlesSpider(scrapy.Spider):
-    name = 'articles'
-    allowed_domains = ['en.wikipedia.org']
-    start_urls = ['https://en.wikipedia.org/wiki/Wikipedia:Featured_articles/']
+    name = "articles"
+    allowed_domains = ["en.wikipedia.org"]
+    start_urls = ["https://en.wikipedia.org/wiki/Wikipedia:Featured_articles"]
 
     def parse(self, response):
-        pass
+
+        host = response.url.split("/wiki")[0]
+
+        content_list = response.css(".hlist > *")
+        looking_for_header = True
+        category = None
+        for element in content_list:
+            if looking_for_header and element.css("h3"):
+                # Get the category of the article by the header
+                looking_for_header = False
+                category = element.css("span::attr(id)").get()
+            elif not looking_for_header and element.css("ul"):
+                # Get the article title
+                # Get the list of the articles
+                article_list = element.css("li")
+                # Get a random article
+                article_index = random.randrange(len(article_list) - 1)
+                title = article_list[article_index].css("a::attr(title)").get()
+                link = host + article_list[article_index].css("a::attr(href)").get()
+                yield DailyWikiItem(category=category, title=title, link=link)
+                category = None
+                looking_for_header = True
+            else:
+                continue
